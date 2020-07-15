fbdf1a4e93d72bf550e360d6f65ddf8928284ab6
黄鸣笛 <63275274+Angelina-magic@users.noreply.github.com>
2020-07-15 22:14:38 +0800

    三点顺序（三个点的顺序问题）
    编程思路：由于该程序要求使用坐标，因此自定义一个坐标有关的数据类型比较好。设计一个与之相关的函数，需要有三个坐标的输入（a， b， c）以及一个可能输出值（0 -->逆时针 或 1-->顺时针 或 -1-->不是三角形）。对于这个问题直接使用定义求向量积就可以得到该向量积的相关公式，即为第19行代码。
    遇到的问题：我在开始的时候以为sin与cos的程序对于这个程序的结果相同于是我直接使用了cos-->sin的程序以此来求结果。但是在我检验的时候我发现程序的结果与正确的结果不同，原因有三：（1）sin在0~Π内为正，也就是说我无论怎么算，结果都是相同的。（2）我在使用余弦定理求两向量夹角的余弦值的时候忽略了除数不能为零的情况，v=。。。/sqrt（。。。）使得程序对于0 0 0 0 0 0这个输入依然判断为一个正确的三角形。（3）我的程序不能对多个数据进行处理。

--- three_points.c
+++ three_points.c
@@ -4,31 +4,23 @@
 //测试数据不超过10000组
 // 如果这三个点是顺时针给出的，请输出1，逆时针给出则输出0
 #include <stdio.h>
-#include <math.h>
 
 typedef struct { double x; double y;} COD;
 
 int three_points(COD a, COD b, COD c) {
     COD ab, bc;
-    double vector_cos, angle, vector_sin;
+    double vector_project;
 
     //将坐标化为向量
     ab.x = b.x - a.x, ab.y = b.y - a.y;
     bc.x = c.x - b.x, bc.y = c.y - b.y;
 
-    //使用余弦定理求cos再求正弦
-    vector_cos = (ab.x*bc.x + ab.y*bc.y)/sqrt((pow(ab.x, 2)+pow(ab.y, 2))*(pow(bc.x, 2)+pow(bc.y, 2)));
-    angle = acos(vector_cos);
-    vector_sin = sin(angle);
-
-    if (vector_sin) {
-        if (vector_sin>0)
-            return 1;
-        else
-            return 0;
-    } else {
-        printf("this isn't a delta!  ");
-        return -1;
+    //直接通过向量积的定义求解
+    vector_project = ab.x * bc.y - ab.y * bc.x;
+    if (vector_project > 0) {
+        return 0;
+    } else if (vector_project < 0) {
+        return 1;
     }
 }
 
@@ -38,6 +30,12 @@ int main() {
     printf("x1 y1 x2 y2 x3 y3: ");
     scanf("%lf %lf %lf %lf %lf %lf", &a.x, &a.y, &b.x, &b.y, &c.x, &c.y);
 
-    printf("%d", three_points(a, b, c));
+    while(a.x||a.y||b.x||b.y||c.x||c.y){
+        printf("%d\n", three_points(a, b, c));
+
+        printf("x1 y1 x2 y2 x3 y3: ");
+        scanf("%lf %lf %lf %lf %lf %lf", &a.x, &a.y, &b.x, &b.y, &c.x, &c.y);
+    }
+
     return 0;
 }
