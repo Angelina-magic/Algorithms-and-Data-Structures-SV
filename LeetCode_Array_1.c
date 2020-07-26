662e91cda766679153f5de66e3f524424029307b
黄鸣笛 <63275274+Angelina-magic@users.noreply.github.com>
2020-07-26 17:17:42 +0800

    leetcode两数相加-c语言
    解题思路：直接进行遍历，相当于双指针法（以后会将hash散列法写出来，不过是在我明白以后）。程序中给出的是相应的一个函数，输入为：nums（输入的数组），numSize（输入数组的长度），target（目标值），returnSize（返回数组的长度）。
    遇到的问题：该方法的时间复杂度过高。

--- LeetCode_Array_1.c
+++ LeetCode_Array_1.c
@@ -1,11 +1,10 @@
-// 暴力算法，通过两边循环实现对每一个数的相加。算法的时间复杂度为O(n!),空间复杂度为O(1)。
+// 暴力算法，通过两边循环实现对每一个数的相加。算法的时间复杂度为O(n^2),空间复杂度为O(1)。
 int* twoSum_BF(int* nums, int numsSize, int target, int* returnSize){
     int i, j;
     int *ret = (int *)malloc(2*sizeof(int));
     for(i = 0; i < numsSize; ++i) {
         for(j = numsSize-1; j > i; --j) {
             if(nums[i]+nums[j]==target) {
-                
                 ret[0] = i, ret[1] = j, *returnSize = 2;
                 return ret;
             }
